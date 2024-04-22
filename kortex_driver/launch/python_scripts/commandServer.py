feaf481e752b6269c153adc13a9c362e7c043b1c
Rishik Tiwari <techyrishik@gmail.com>
2024-04-22 16:57:31 +1000

    cmd srvr connected to kinova driver

--- kortex_driver/launch/python_scripts/commandServer.py
+++ kortex_driver/launch/python_scripts/commandServer.py
@@ -1,51 +1,87 @@
 #!/usr/bin/env python
 
-import rospy
-from std_msgs.msg import String
 import socket
+import threading
+# import rospy
+import numpy as np
+from kinova import KinovaControls
 
-HOST_ADDR = ('0.0.0.0', 9999)
-ENCODING_FORMAT = 'utf-8'
-HEADER_SIZE = 1024
+class CommandServer:
+    def __init__(self):
+        self.HOST_ADDR = ('0.0.0.0', 9999)
+        self.ENCODING_FORMAT = 'utf-8'
+        self.HEADER_SIZE = 1024
+        
+        self.isRunning = True
+        self.rcvmsg_socket = None
+        self.client_socket = None
+        self.client_addr = None
 
-subscriberNode = 'rishik_commander'
-topicName = '/my_gen3/joint_states'
-# rospy.init_node(subscriberNode, anonymous=True)
-# rospy.Subscriber(topicName, String, sendStateData) # TODO: create publisher
-# rospy.spin()
+        self.kinovactrl = KinovaControls()
+        # self.subscriberNode = 'rishik_commander'
+        # self.topicName = '/my_gen3/joint_states'
+        # rospy.init_node(subscriberNode, anonymous=True)
+        # rospy.Subscriber(topicName, String, sendStateData) # TODO: create publisher
+        # rospy.spin()
 
-print('Starting command link...')
 
-rcvmsg_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #TCP
-rcvmsg_socket.bind(HOST_ADDR)
-rcvmsg_socket.listen(5)
-
-print('Command link listening at', HOST_ADDR)
-
-(client_socket, client_addr) = rcvmsg_socket.accept()
-
-if client_socket:
-    print('Client connected', client_addr)
-    while True:
+    def initServer(self):
+        print('Starting command link...')
         try:
-            msg = client_socket.recv(HEADER_SIZE).decode(encoding=ENCODING_FORMAT)
-            if (msg == ''):
-                continue
-            elif(msg == 'quit'):
-                print('> QUIT RCVD <')
+            self.rcvmsg_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #TCP
+            self.rcvmsg_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.rcvmsg_socket.bind(self.HOST_ADDR)
+            self.rcvmsg_socket.listen(5)
+            print('Command link listening at', self.HOST_ADDR)
+
+            (self.client_socket, self.client_addr) = self.rcvmsg_socket.accept()
+            if self.client_socket:
+                print('Client connected', self.client_addr)
+                threading.Thread(target=self._recvMsgs).start()
+                self.kinovactrl.initContinousListener()
+        
+        except KeyboardInterrupt:
+            print('KbdInrpt, closing socket')
+            self.closeSocket()
+
+
+    def _recvMsgs(self):
+        while self.isRunning:
+            try:
+                msg = self.client_socket.recv(self.HEADER_SIZE).decode(encoding=self.ENCODING_FORMAT)
+                if (msg == ''):
+                    continue
+                elif(msg == 'quit'):
+                    print('> QUIT RCVD <')
+                    break
+                print('RCVD CMD: \n>\t%s' % msg)
+
+                self.kinovactrl.addPoseToQueue(msg)
+                # cartCoords = msg.split(' ')
+                # cartCoords = np.asarray(cartCoords, dtype=float)
+                # self.kinovactrl.move_cartesian(*cartCoords)
+
+            except Exception or KeyboardInterrupt as e:
+                print(e)
+                print('Exception occured, closing socket')
                 break
-            
-            print('RCVD CMD: \n>\t%s' % msg)
 
-        except Exception or KeyboardInterrupt:
-            print('Exception occured, closing socket')
-            break
+        self.closeSocket()
 
 
-print('Quitting...')
-client_socket.shutdown(socket.SHUT_RDWR)
-rcvmsg_socket.shutdown(socket.SHUT_RDWR)
-client_socket.close()
-rcvmsg_socket.close()
-# rospy.signal_shutdown('')
-print('Command link closed')
+    def closeSocket(self):
+        print('Quitting...')
+        self.isRunning = False
+        self.kinovactrl.closeKinova()
+        self.client_socket.shutdown(socket.SHUT_RDWR)
+        self.rcvmsg_socket.shutdown(socket.SHUT_RDWR)
+        self.client_socket.close()
+        self.rcvmsg_socket.close()
+        # rospy.signal_shutdown('')
+        print('Command link closed')
+
+
+
+if __name__ == '__main__':
+    cmdSrv = CommandServer()
+    cmdSrv.initServer()
---
+++ kortex_driver/launch/python_scripts/kinova.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+
+import rospy
+import time
+import threading
+import numpy as np
+from Queue import Queue
+
+from std_msgs.msg import String
+from kortex_driver.srv import *
+from kortex_driver.msg import *
+
+class KinovaControls:
+    def __init__(self):
+        self.cmdQueue = Queue()
+        self.isRunning = True
+
+        try:
+            rospy.init_node('kinova_rishik')
+
+            self.HOME_ACTION_IDENTIFIER = 2
+
+            self.action_topic_sub = None
+            self.all_notifs_succeeded = True
+            self.poseId = 1
+
+            # Get node params
+            self.robot_name = rospy.get_param('~robot_name', "my_gen3")
+            self.degrees_of_freedom = rospy.get_param("/" + self.robot_name + "/degrees_of_freedom", 7)
+            self.is_gripper_present = rospy.get_param("/" + self.robot_name + "/is_gripper_present", False)
+
+            rospy.loginfo("Using robot_name " + self.robot_name)
+
+            # Init the action topic subscriber
+            self.action_topic_sub = rospy.Subscriber("/" + self.robot_name + "/action_topic", ActionNotification, self.cb_action_topic)
+            self.last_action_notif_type = None
+
+            # Init the services
+            clear_faults_full_name = '/' + self.robot_name + '/base/clear_faults'
+            rospy.wait_for_service(clear_faults_full_name)
+            self.clear_faults = rospy.ServiceProxy(clear_faults_full_name, Base_ClearFaults)
+
+            read_action_full_name = '/' + self.robot_name + '/base/read_action'
+            rospy.wait_for_service(read_action_full_name)
+            self.read_action = rospy.ServiceProxy(read_action_full_name, ReadAction)
+
+            execute_action_full_name = '/' + self.robot_name + '/base/execute_action'
+            rospy.wait_for_service(execute_action_full_name)
+            self.execute_action = rospy.ServiceProxy(execute_action_full_name, ExecuteAction)
+
+            set_cartesian_reference_frame_full_name = '/' + self.robot_name + '/control_config/set_cartesian_reference_frame'
+            rospy.wait_for_service(set_cartesian_reference_frame_full_name)
+            self.set_cartesian_reference_frame = rospy.ServiceProxy(set_cartesian_reference_frame_full_name, SetCartesianReferenceFrame)
+
+            send_gripper_command_full_name = '/' + self.robot_name + '/base/send_gripper_command'
+            rospy.wait_for_service(send_gripper_command_full_name)
+            self.send_gripper_command = rospy.ServiceProxy(send_gripper_command_full_name, SendGripperCommand)
+
+            activate_publishing_of_action_notification_full_name = '/' + self.robot_name + '/base/activate_publishing_of_action_topic'
+            rospy.wait_for_service(activate_publishing_of_action_notification_full_name)
+            self.activate_publishing_of_action_notification = rospy.ServiceProxy(activate_publishing_of_action_notification_full_name, OnNotificationActionTopic)
+        except:
+            self.is_init_success = False
+        else:
+            self.is_init_success = True
+        
+        success = self.is_init_success
+        success &= self.clear_errs()
+        # success &= self.home_the_robot()
+        success &= self.setReferenceToCartesian()
+        success &= self.subscribeRobotNotification()
+
+    def cb_action_topic(self, notif):
+        self.last_action_notif_type = notif.action_event
+
+    def wait_for_action_end_or_abort(self):
+        while not rospy.is_shutdown():
+            if (self.last_action_notif_type == ActionEvent.ACTION_END):
+                rospy.loginfo("Received ACTION_END notification")
+                return True
+            elif (self.last_action_notif_type == ActionEvent.ACTION_ABORT):
+                rospy.loginfo("Received ACTION_ABORT notification")
+                self.all_notifs_succeeded = False
+                return False
+            else:
+                time.sleep(0.01)
+
+    def clear_errs(self):
+        try:
+            self.clear_faults()
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call ClearFaults")
+            return False
+        else:
+            rospy.loginfo("Cleared the faults successfully")
+            rospy.sleep(2.5)
+            return True
+
+    def home_the_robot(self):
+        # The Home Action is used to home the robot. It cannot be deleted and is always ID #2:
+        req = ReadActionRequest()
+        req.input.identifier = self.HOME_ACTION_IDENTIFIER
+        self.last_action_notif_type = None
+        try:
+            res = self.read_action(req)
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call ReadAction")
+            return False
+        # Execute the HOME action if we could read it
+        else:
+            # What we just read is the input of the ExecuteAction service
+            req = ExecuteActionRequest()
+            req.input = res.output
+            rospy.loginfo("Sending the robot home...")
+            try:
+                self.execute_action(req)
+            except rospy.ServiceException:
+                rospy.logerr("Failed to call ExecuteAction")
+                return False
+            else:
+                return self.wait_for_action_end_or_abort()
+
+    def setReferenceToCartesian(self):
+        # Prepare the request with the frame we want to set
+        req = SetCartesianReferenceFrameRequest()
+        req.input.reference_frame = CartesianReferenceFrame.CARTESIAN_REFERENCE_FRAME_MIXED
+
+        # Call the service
+        try:
+            self.set_cartesian_reference_frame()
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call SetCartesianReferenceFrame")
+            return False
+        else:
+            rospy.loginfo("Set the cartesian reference frame successfully")
+            return True
+
+
+    def subscribeRobotNotification(self):
+        # Activate the publishing of the ActionNotification
+        req = OnNotificationActionTopicRequest()
+        rospy.loginfo("Activating the action notifications...")
+        try:
+            self.activate_publishing_of_action_notification(req)
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call OnNotificationActionTopic")
+            return False
+        else:
+            rospy.loginfo("Successfully activated the Action Notifications!")
+
+        rospy.sleep(1.0)
+
+        return True
+    
+
+    def actuate_gripper(self, value):
+        # Initialize the request
+        # Close the gripper
+
+        if(not self.is_gripper_present):
+           print("Gripper not detected!")
+           return False
+
+        req = SendGripperCommandRequest()
+        finger = Finger()
+        finger.finger_identifier = 0
+        finger.value = value
+        req.input.gripper.finger.append(finger)
+        req.input.mode = GripperMode.GRIPPER_POSITION
+
+        rospy.loginfo("Sending the gripper command...")
+
+        # Call the service 
+        try:
+            self.send_gripper_command(req)
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call SendGripperCommand")
+            return False
+        else:
+            return True
+
+    def move_cartesian(self, px, py, pz, gripperVal = 0.0):
+        # For testing purposes
+        success = self.is_init_success
+
+        if success:
+            success &= self.clear_errs()
+            # success &= self.home_the_robot()
+            # success &= self.setReferenceToCartesian()
+            # success &= self.subscribeRobotNotification()
+
+            # POSITION DEFINITION
+            my_cartesian_speed = CartesianSpeed()
+            my_cartesian_speed.translation = 0.2 # m/s
+            my_cartesian_speed.orientation = 15  # deg/s
+
+            try:
+                if(((px**2) + (py**2) + (pz**2))**0.5 >= 0.9):
+                    print("Infeasible pose!")
+                    return False
+                
+                # actuate gripper
+                print('actuating gripper')
+                self.actuate_gripper(gripperVal)
+                time.sleep(1) # required for action to complete
+                
+                print('preparing pose')
+                my_constrained_pose = ConstrainedPose()
+                my_constrained_pose.target_pose.x = px
+                my_constrained_pose.target_pose.y = py
+                my_constrained_pose.target_pose.z = pz
+                my_constrained_pose.target_pose.theta_x = 0     # 0deg - points down
+                my_constrained_pose.target_pose.theta_y = 180   # 180deg - camera is outward and straight
+                my_constrained_pose.target_pose.theta_z = 0     # 0deg - points toward +ve y-axis
+                my_constrained_pose.constraint.oneof_type.speed.append(my_cartesian_speed)
+
+                req = ExecuteActionRequest()
+                req.input.oneof_action_parameters.reach_pose.append(my_constrained_pose)
+                req.input.name = "pose" + str(self.poseId)
+                req.input.handle.action_type = ActionType.REACH_POSE
+                req.input.handle.identifier = 1000 + self.poseId
+
+                rospy.loginfo("Sending pose " + str(self.poseId) + "...")
+                self.last_action_notif_type = None
+                try:
+                    self.execute_action(req)
+                except rospy.ServiceException:
+                    rospy.logerr("Failed to send pose "+str(self.poseId))
+                    success = False
+                else:
+                    rospy.loginfo("Waiting for pose to finish...")
+
+                # self.wait_for_action_end_or_abort()
+                self.poseId += 1
+
+            except KeyboardInterrupt:
+                print('Terminating...')
+                self.isRunning = False
+                pass
+
+        if not success:
+            rospy.logerr("The program encountered an error.")
+            self.isRunning = False
+        
+
+    def _startMoveThread(self):
+        while self.isRunning:
+            if self.cmdQueue.empty():
+                continue
+            
+            poseStr = self.cmdQueue.get()
+            cartCoords = poseStr.split(' ')
+            cartCoords = np.asarray(cartCoords, dtype=float)
+            self.move_cartesian(*cartCoords)
+
+
+    def addPoseToQueue(self, pose):
+        self.cmdQueue.put(pose)
+        return None
+
+
+    def initContinousListener(self):
+        threading.Thread(target=self._startMoveThread, args=()).start()
+        return True
+
+
+    def closeKinova(self):
+        print('Closing kinova controller')
+        self.isRunning = False
+        with self.cmdQueue.mutex:
+            self.cmdQueue.queue.clear()
